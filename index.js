009fda4071acc793e26c521e0d8de2771c7d91a1
Shakib AL Nur <shakibalnur3268@gmail.com>
2023-06-12 02:15:27 +0600

    admin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 // middleware
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.48shya3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -41,6 +42,18 @@ async function run() {
         }
         const result = await usersCollection.insertOne(user)
         res.send(result)
+    })
+
+    app.patch('/users/admin/:id',async (req,res) =>{
+        const id = req.params.id ;
+        const filter = { _id: new ObjectId(id)}
+        const updateDoc = {
+            $set: {
+              role: 'admin'
+            },
+          };
+          const result = await usersCollection.updateOne(filter,updateDoc)
+          res.send(result)
     })
 
     // Send a ping to confirm a successful connection
